c3e8945d993b6cd9b3c301f36a6dc15cac7ac8e1
rjspyk5 <rjspyk5@gmail.com>
2024-03-17 01:15:00 +0600

    data made and data fetching complete

--- src/Components/Main/Recipe/Recipe.jsx
+++ src/Components/Main/Recipe/Recipe.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { RecipeHeading } from "./RecipeHeading";
-RecipeHeading;
+import { ItemCard } from "./ItemCard/ItemCard";
 
 export const Recipe = () => {
+  const [recipeData, setrecipeData] = useState([]);
+  useEffect(() => {
+    const f = async () => {
+      const promisedData = await fetch("data.json");
+      const actualData = await promisedData.json();
+      setrecipeData([...actualData]);
+    };
+    f();
+  }, []);
+
   return (
     <div>
       <RecipeHeading></RecipeHeading>
+      <div className="grid grid-cols-1 lg:grid-cols-10">
+        <div className="lg:col-span-6 order-2 lg:order-1">h</div>
+        <div className="order-1 lg:order-2"> </div>
+      </div>
     </div>
   );
 };
