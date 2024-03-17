62cd54304e1e3b51e03dd00c1392b80a0edad11c
rjspyk5 <rjspyk5@gmail.com>
2024-03-17 20:10:43 +0600

    responsive navbar complete

--- src/Components/Header/Banner/Banner.jsx
+++ src/Components/Header/Banner/Banner.jsx
@@ -4,26 +4,29 @@ import chefSalmon from "../../../assets/img/chef-photo=one.jpg";
 export const Banner = () => {
   return (
     <div
-      className="hero min-h-[480px] rounded-3xl"
+      className="hero min-h-[300px] md:min-h-[500px] bg-contain rounded-3xl"
       style={{
-        backgroundImage:
-          "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
+        backgroundImage: `url(${chefSalmon})`,
       }}
     >
-      <div className="hero-overlay bg-opacity-60 rounded-3xl"></div>
-      <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-4xl">
-          <h1 className="mb-5 text-5xl font-bold">
+      <div className=" h-full w-full bg-[black] opacity-50 rounded-3xl"></div>
+      <div className="hero-content p-5 text-center text-neutral-content">
+        <div className="max-w-xl md:max-w-4xl">
+          <h1 className="mb-5 text-2xl lg:text-5xl font-bold">
             Discover an exceptional cooking class tailored for you!
           </h1>
           <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            Unlock your culinary creativity with bespoke cooking classes
+            designed to suit your taste preferences, skill level, and culinary
+            aspirations.
           </p>
           <div className="flex gap-4 justify-center">
-            <button className="btn btn-primary">Get Started</button>
-            <button className="btn btn-primary">Get Started</button>
+            <button className="btn border-none rounded-full bg-main">
+              Explore Now
+            </button>
+            <button className="btn rounded-full bg-transparent text-[white]">
+              Our Feedback
+            </button>
           </div>
         </div>
       </div>
